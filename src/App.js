feb405497f8c24593f15a32b63a4a3572bdcc613
Gaurav kashyap <kashyapgaurav460@gmail.com>
2023-08-14 15:20:02 +0530

    client side of editor page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Editorpage from "./pages/Editorpage";
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/Home';
+import Editorpage from './pages/Editorpage';
+
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/editor/:roomId" element={<Editorpage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Toaster
+                    position="top-right"
+                    toastOptions={{
+                        success: {
+                            theme: {
+                                primary: '#4aed88',
+                            },
+                        },
+                    }}
+                ></Toaster>
+            </div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route
+                        path="/editor/:roomId"
+                        element={<Editorpage />}
+                    ></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- src/pages/Editorpage.js
+++ src/pages/Editorpage.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import Client from '../components/Client'
 
 const Editorpage = () => {
   return (
-    <div>This is Editorpage</div>
+    <div className="mainWrap">
+            <div className="aside">
+                <div className="asideInner">
+                    <div className="logo">
+                        <img
+                            className="logoImage"
+                            src="/code-sync.png"
+                            alt="logo"
+                        />
+                    </div>
+                    <h3>Connected</h3>
+                    <div className="clientsList">
+                        {clients.map((client) => (
+                            <Client
+                                key={client.socketId}
+                                username={client.username}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <button className="btn copyBtn" onClick={copyRoomId}>
+                    Copy ROOM ID
+                </button>
+                <button className="btn leaveBtn" onClick={leaveRoom}>
+                    Leave
+                </button>
+            </div>
+            <div className="editorWrap">
+
+            </div>
+        </div>
   )
 }
 
